b88f333fd7c57d5cb450835cef47edf4f589bded
Michael <zwang20@knox.nsw.edu.au>
2018-09-12 19:23:18 +1000

    No

--- virtual_stellaris.py
+++ virtual_stellaris.py
@@ -1,19 +1,34 @@
+import random
 
-class Planet:
-    objects = []
+# class Planet:
+#     objects = []
+#
+#     def __init__(self, name, pp, up):
+#         self.name = name
+#         self.pp = pp
+#         self.up = up
+#         Planet.objects.append(self)
 
-    def __init__(self, name, pp, up):
-        self.name = name
-        self.pp = pp
-        self.up = up
-        Planet.objects.append(self)
+random_names = open('names.txt', 'r').read().split('\n')
 
+planet_dict = {
+    '' : [0, 0],
+    'planet' : [1, 0],
+    'asteroid' : [3, 0],
+    'gas_giant' : [0, 1],
+}
 
-        
+planet_type_list = [key for key in planet_dict.keys()]
+print (planet_type_list)
+
 class System:
     objects = []
 
-    def __init__(self, name, description, p1, p2, p3, p4, p5, p6, p7, p8,):
+    def __init__(self, name, description):
         self.name = name
         self.discription = description
+        self.planets = [random.choice(planet_type_list) for i in range(8)]
+        print (self.planets)
         System.objects.append(self)
+
+System('Test', 'Blame Edward')
