f8c7280a307ca8d649afdc0fde2ddd95b46449bb
Michael <zwang20@knox.nsw.edu.au>
2018-09-12 19:38:27 +1000

    No

--- virtual_stellaris.py
+++ virtual_stellaris.py
@@ -19,16 +19,41 @@ planet_dict = {
 }
 
 planet_type_list = [key for key in planet_dict.keys()]
-print (planet_type_list)
 
 class System:
     objects = []
+    object_names = []
 
-    def __init__(self, name, description):
-        self.name = name
+    def __init__(self):
+        self.discription = ''
+        temp_name = random.choice(random_names)
+        while temp_name in System.objects:
+            temp_name = random.choice(random_names)
+        self.name = temp_name
+        del temp_name
         self.discription = description
         self.planets = [random.choice(planet_type_list) for i in range(8)]
         print (self.planets)
         System.objects.append(self)
 
-System('Test', 'Blame Edward')
+    def renew():
+        temp_list = []
+        for i in System.objects:
+            temp_list.append(i.name)
+        object_names = temp_list
+        del temp_list
+
+    def modify_name(self, name):
+        if name not in System.objects():
+            self.name = name
+
+    def modify_discription(self, description):
+        self.discription = discription
+
+class PlayerFleet:
+    objects = []
+
+    def __init__(self):
+        pass
+
+System.renew()
