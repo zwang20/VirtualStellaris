f2ca478dea9f42576363c7ee188814d82c7cf6a0
Michael <zwang20@knox.nsw.edu.au>
2019-03-06 20:39:57 +1100

    Things

--- stellaris_v2.0.0.py
+++ stellaris_v2.0.0.py
@@ -1,1 +1,24 @@
 print('Hello')
+
+# Classes
+class System:
+
+    def init(self):
+        pass
+
+
+class SystemObject:
+
+    def init(self):
+        pass
+
+
+# Functions
+pass
+
+# Game
+def main():
+    pass
+
+# execute game
+main()
