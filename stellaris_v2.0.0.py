a1aed36532a8f77f50fed89b6869a3177a505120
Michael <zwang20@knox.nsw.edu.au>
2019-03-06 21:26:22 +1100

    Finally

--- stellaris_v2.0.0.py
+++ stellaris_v2.0.0.py
@@ -1,29 +1,69 @@
+import random
+
 print('Hello')
 
-object_types = []
+object_types = ['Star', 'Planet', 'Astroids', 'Gas Giant']
 
 # Classes
 class StarSystem:
 
-    def init(self):
-        pass
+    def __init__(self):
+
+        self.objects = [StarSystemObjectSun()]
+
+        for x in range(7):
+            if random.randint(0, 1) == 0:
+                self.objects.append(StarSystemObjectPlanet())
+            else:
+                self.objects.append(StarSystemObjectNothing())
+
+    def view(self):
+        for x in self.objects:
+            x.view()
 
 
 class StarSystemObject:
 
-    def init(self, object_type, production_points, upkeep_points):
-        self.object_type = object_type
-        self.production_points = production_points
-        self.upkeep_points = upkeep_points
+    upkeep_points = 0
+    production_points = 0
+
+    def __init__(self):
         pass
 
+    def view(self):
+        print('upkeep_points,', self.upkeep_points, 'production_points,', self.production_points)
+
+
+class StarSystemObjectSun(StarSystemObject):
+
+    upkeep_points = 1
+    production_points = 0
+
+    def __init__(self):
+        super().__init__()
+
+
+class StarSystemObjectPlanet(StarSystemObject):
+
+    upkeep_points = 0
+    production_points = 1
+
+    def __init__(self):
+        super().__init__()
+
+
+class StarSystemObjectNothing(StarSystemObject):
+
+    upkeep_points = 0
+    production_points = 0
 
 # Functions
 pass
 
 # Game
 def main():
-    pass
+    sys1 = StarSystem()
+    sys1.view()
 
 # execute game
 main()
